f2e549365cfae632cc5a98178e9a90b92b8738fd
Pavelcaj <mr.derkach2000@mail.ru>
2023-10-22 10:32:34 +0300

    Add lesson 22 homework 2

---
+++ src/main/java/Lesson22/hw2/AgeCheckServlet.java
@@ -0,0 +1,23 @@
+package Lesson22.hw2;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ Создать сервлет который на вход принимает возраст, а в ответе возвращает
+ информацию, совершеннолетний или нет .
+ */
+@WebServlet("/age-check")
+public class AgeCheckServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        if (age >= 18) {
+            response.getWriter().println("Adult");
+        } else {
+            response.getWriter().println("Minor");
+        }
+    }
+}
---
+++ target/c24-onl-part2/ageForm.jsp
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>Input your age</title>
+</head>
+<body>
+<h1>Input your age:</h1>
+<input type="text" id="ageInput">
+<button onclick="submitAge()">confirm</button>
+<p id="result"></p>
+
+<script>
+    function submitAge() {
+        var age = document.getElementById("ageInput").value;
+        if (age === "" || isNaN(age)) {
+            document.getElementById("result").innerHTML = "Please enter the correct age.";
+        } if(age>=18) {
+            document.getElementById("result").innerHTML = "Adult";
+        }else{
+            document.getElementById("result").innerHTML = "Minor";
+        }
+    }
+</script>
+</body>
+</html>
