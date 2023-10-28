8e3b2f9bc59250767eaa9c0078a3b8644f7a84d6
Pavelcaj <mr.derkach2000@mail.ru>
2023-10-28 14:02:41 +0300

    Add lesson 22 homework 1 fixed

--- src/main/java/Lesson22/hw2/AgeCheckServlet.java
+++ src/main/java/Lesson22/hw2/AgeCheckServlet.java
@@ -1,23 +1,24 @@
 package Lesson22.hw2;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
- Создать сервлет который на вход принимает возраст, а в ответе возвращает
- информацию, совершеннолетний или нет  .
+ * Создать сервлет который на вход принимает возраст, а в ответе возвращает
+ * информацию, совершеннолетний или нет  .
  */
 @WebServlet("/age-check")
 public class AgeCheckServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int age = Integer.parseInt(request.getParameter("age"));
+       ServletAgeCheckLogick.postLogickAgeCheck(request,response);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+ServletAgeCheckLogick.getLogickAgeCheck(request, response);
 
-        if (age >= 18) {
-            response.getWriter().println("Adult");
-        } else {
-            response.getWriter().println("Minor");
-        }
     }
 }
