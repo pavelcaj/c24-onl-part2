0dd17aa858a2a0435e70a018424eb2dc712e7e8a
Pavelcaj <mr.derkach2000@mail.ru>
2024-01-23 18:49:07 +0300

    Add lesson 43 homework

--- spring-mvc/src/main/java/lesson43/dao/PersonDAO.java
+++ spring-mvc/src/main/java/lesson43/dao/PersonDAO.java
@@ -3,60 +3,47 @@ package lesson43.dao;
 import lesson43.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
+@Transactional
 public class PersonDAO {
 
-    private final SessionFactory factory;
+    private final SessionFactory sessionFactory;
 
-    public PersonDAO() {
-        Configuration configuration = new Configuration().configure();
-        factory = configuration.buildSessionFactory();
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     public void addPerson(Person person) {
-        try (Session session = factory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.save(person);
-            transaction.commit();
-        }
+        getCurrentSession().save(person);
     }
 
-    public Person getPersonById(int id) {
-        try (Session session = factory.openSession()) {
-            return session.get(Person.class, id);
-        }
+    public Person getPersonById(Long id) {
+        return getCurrentSession().get(Person.class, id);
     }
 
     public List<Person> getAllPersons() {
-        try (Session session = factory.openSession()) {
-            return session.createQuery("FROM Person", Person.class).list();
-        }
+        return getCurrentSession().createQuery("FROM Person", Person.class).list();
     }
 
     public void updatePerson(Person person) {
-        try (Session session = factory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.update(person);
-            transaction.commit();
+        getCurrentSession().update(person);
+    }
+
+    public void deletePerson(Long id) {
+        Person person = getPersonById(id);
+        if (person != null) {
+            getCurrentSession().delete(person);
         }
     }
 
-    public void deletePerson(int id) {
-        try (Session session = factory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Person person = session.get(Person.class, id);
-            if (person != null) {
-                session.delete(person);
-            }
-            transaction.commit();
-        }
-    }
-
-    public void close() {
-        factory.close();
+    private Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
     }
 }
--- spring-mvc/src/main/java/lesson43/model/Person.java
+++ spring-mvc/src/main/java/lesson43/model/Person.java
@@ -3,12 +3,13 @@ package lesson43.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "person")
+@Table(name = "persons")
 public class Person {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
@@ -16,24 +17,24 @@ public class Person {
     @Column(name = "age")
     private int age;
 
-    public int getId() {
+    public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public void setAge(int age) {
